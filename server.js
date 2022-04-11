ca4df334cec42f61b64c6063eeab4970cfbeef42
JWViking <julianawilcox@gmail.com>
2022-04-10 21:47:04 -0600

    queries and inquirer prompt

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mysql = require('msql2');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 require('console.table');
 
@@ -11,13 +11,14 @@ const db = mysql.createConnection (
     }
 );
 
+//USER PROMPTS
 const options = () => {
     inquirer
         .prompt([
             {
                 type: 'list',
                 name: 'choices',
-                message: 'What would you like to access?',
+                message: 'What would you like to select?',
                 choices: [
                     'view all departments',
                     'view all roles',
@@ -31,13 +32,54 @@ const options = () => {
         ])
         .then ((answers) => {
             if (answers.choices === 'view all departments') {
-
+                viewDepartments();
+            } 
+            else if (answers.choices === 'view all roles') {
+                viewRoles();
             }
-        })
-}
+            else if (answers.choices === 'view all employees') {
+                viewEmployees();
+            }
+            else if (answers.choices === 'add a department') {
+                
+                promptDepartment = () => {
+                    inquirer.prompt ([
+                        { 
+                            type: 'input',
+                            name: 'departmentName',
+                            message: 'What is the name of the new department?'
+                        }
+                    ])
+                    .then ((answers) => {
+                        if(answers.departmentName) {
+                            addDepartment(answers.departmentName);
+                        } else {
+                            console.log ('Please enter a new department name.');
+                            promptDepartment();
+                        }
 
+                    })
+                }
+            
+            }
+            else if (answers.choices === 'add a role') {
+                addRole();
+            }
+            else if (answers.choices === 'add an employee') {
+                addEmployee();
+            }
+            else if (answers.choices === 'update an employee role') {
+                updateEmployeeRole();
+            }
+            else {
+                console.log('Please make an appropriate choice from the menu.')
+            }
+        });
+};
+
+//VIEW ALL DEPARTMENTS
 const viewDepartments = () => {
-    db.query(`SELECT * FROM DEPARTMENT`,(err, data) => {
+    db.query(`SELECT * FROM department`,(err, data) => {
         if(err) {
             throw err
         } else {
@@ -45,3 +87,39 @@ const viewDepartments = () => {
         }
         options();
     })};
+
+//VIEW ALL ROLES
+const viewRoles = () => {
+    db.query(`SELECT * FROM role`, (err, data) => {
+        if(err) {
+            throw err
+        } else {
+            console.table(data)
+        }
+        options();
+    })
+}; 
+
+//VIEW ALL EMPLOYEES
+const viewEmployees = () => {
+    db.query(`SELECT * FROM employee`, (err, data) => {
+        if(err) {
+            throw err
+        } else {
+            console.table(data)
+        }
+        options();
+    })
+};
+
+//ADD A DEPARTMENT
+const addDepartment = (answers.departmentName) => {
+    db.query(`INSERT INTO department (department.name) VALUES(answers.departmentName)`, (err, data) => {
+        if(err) {
+            throw err
+        } else {
+            console.table(data)
+        }
+        options();
+    })
+};
